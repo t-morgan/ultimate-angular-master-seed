2882a77ee3013c54587b993b3a40c32d83112e11
Tim Morgan <morgat3@nationwide.com>
2017-01-09 11:44:21 -0500

    Using a workaround for contactsFilter

--- src/app/components/contact/contacts/contacts.controller.js
+++ src/app/components/contact/contacts/contacts.controller.js
@@ -1,14 +1,16 @@
 function ContactsController($filter, $state) {
   var ctrl = this;
   var contacts = ctrl.contacts;
+  ctrl.contactsFilter = $filter('contactsFilter');
+
+  // The line below is supposed to work... and yet it does not
+  //ctrl.filteredContacts = $filter('contactsFilter')(contacts, ctrl.filter);
 
   ctrl.goToContact = function (event) {
     $state.go('contact', {
       id: event.contactId
     });
   };
-
-  ctrl.filteredContacts = $filter('contactsFilter')(contacts, ctrl.filter);
 }
 
 angular
