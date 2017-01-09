b2fb7fdbe34b6a6f02c0be3ecdb8561e4bb5065f
Tim Morgan <morgat3@nationwide.com>
2017-01-09 15:27:32 -0500

    Fixing contacts page with angular 1.5.*

--- src/app/components/contact/contacts/contacts.controller.js
+++ src/app/components/contact/contacts/contacts.controller.js
@@ -1,10 +1,8 @@
 function ContactsController($filter, $state) {
   var ctrl = this;
   var contacts = ctrl.contacts;
-  ctrl.contactsFilter = $filter('contactsFilter');
 
-  // The line below is supposed to work... and yet it does not
-  //ctrl.filteredContacts = $filter('contactsFilter')(contacts, ctrl.filter);
+  ctrl.filteredContacts = $filter('contactsFilter')(contacts, ctrl.filter);
 
   ctrl.goToContact = function (event) {
     $state.go('contact', {
