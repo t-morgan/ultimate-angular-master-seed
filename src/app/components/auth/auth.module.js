6d88e870fcb3833d8b15c4ab49d0d702bf823e55
Tim Morgan <tmthy.morgan@gmail.com>
2017-01-08 21:34:33 -0500

    almost done with videos

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,38 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function ($firebaseRefProvider) {
+    // Initialize Firebase
+    var config = {
+      // Get this from Firebase
+    };
+
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        contacts: config.databaseURL + '/contacts'
+      });
+
+    firebase.initializeApp(config);
+  })
+  .run(function ($transitions, $state, AuthService) {
+    $transitions.onStart({
+      to: function (state) {
+        return !!(state.data && state.data.requiredAuth);
+      }
+    }, function() {
+      return AuthService
+        .requireAuthentication()
+        .catch(function () {
+          return $state.target('auth.login');
+        });
+    });
+    $transitions.onStart({
+      to: 'auth.*'
+    }, function () {
+      if (AuthService.isAuthenticated()) {
+        return $state.target('app');
+      }
+    });
+  });
